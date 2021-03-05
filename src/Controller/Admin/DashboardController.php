fcd3014f319fd739b117ff4d5850e3cd6e9d3212
WainlaiN <nicodupriez@hotmail.com>
2021-03-05 22:59:14 +0100

    Added all CRUD to easyadmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,8 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\BlogPost;
 use App\Entity\Project;
+use App\Entity\Skill;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Menu\RouteMenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -32,12 +34,21 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+
         yield MenuItem::section('Users');
         yield MenuItem::linkToCrud('User', 'fas fa-users', User::class);
+
         yield MenuItem::section('Blogs');
         yield MenuItem::linkToCrud('BlogPost', 'fas fa-newspaper', BlogPost::class);
+
         yield MenuItem::section('Projets');
-        yield MenuItem::linkToCrud('Project', ' fas fa-project-diagram', Project::class);
-        //yield MenuItem::linkToCrud('Skill', 'fas fa-list', Skill::class);
+        yield MenuItem::linkToCrud('Project', 'fas fa-project-diagram', Project::class);
+
+        yield MenuItem::section('Skills');
+        yield MenuItem::linkToCrud('Skill', 'fas fa-tasks', Skill::class);
+
+        yield MenuItem::linkToRoute('Home', 'fas fa-home', 'home');
+        
+        
     }
 }
