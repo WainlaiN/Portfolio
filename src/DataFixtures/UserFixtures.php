912b77a82c381cd39112c517f07d2966cc665f8c
Nicolas <nicodupriez@hotmail.com>
2021-03-06 11:57:10 +0100

    Working on persist database with docker

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+
+    private UserPasswordEncoderInterface $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail("admin@gmail.com")
+            ->setPassword($this->encoder->encodePassword($user, "test"))
+            ->setPrenom("nicolas")
+            ->setNom("dup");
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
