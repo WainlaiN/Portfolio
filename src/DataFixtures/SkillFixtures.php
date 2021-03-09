e12096964ff6f18f98499e0e076f8c7c51971cd3
WainlaiN <nicodupriez@hotmail.com>
2021-03-09 12:11:09 +0100

    Fix home design

--- src/DataFixtures/SkillFixtures.php
+++ src/DataFixtures/SkillFixtures.php
@@ -38,19 +38,59 @@ class SkillFixtures extends Fixture implements DependentFixtureInterface
 
         $manager->persist($skill2);
 
-        $skill3 = new Skill();
-        $skill3->setTitle("HTML5")
-            ->setPercentage(70)
+        $skill21 = new Skill();
+        $skill21->setTitle("PHP")
+            ->setPercentage(60)
             ->setUser($user)
             ->setNumber(4);
 
+        $manager->persist($skill21);
+
+        $skill22 = new Skill();
+        $skill22->setTitle("MySQL")
+            ->setPercentage(60)
+            ->setUser($user)
+            ->setNumber(5);
+
+        $manager->persist($skill22);
+
+        $skill23 = new Skill();
+        $skill23->setTitle("Twig")
+            ->setPercentage(80)
+            ->setUser($user)
+            ->setNumber(6);
+
+        $manager->persist($skill23);
+
+        $skill24 = new Skill();
+        $skill24->setTitle("Git")
+            ->setPercentage(70)
+            ->setUser($user)
+            ->setNumber(7);
+
+        $manager->persist($skill24);
+
+        $skill3 = new Skill();
+        $skill3->setTitle("Symfony")
+            ->setPercentage(70)
+            ->setUser($user)
+            ->setNumber(8);
+
         $manager->persist($skill3);
+
+        $skill31 = new Skill();
+        $skill31->setTitle("Gitlab CI")
+            ->setPercentage(40)
+            ->setUser($user)
+            ->setNumber(9);
+
+        $manager->persist($skill31);
 
         $skill4 = new Skill();
         $skill4->setTitle("Linux")
             ->setPercentage(60)
             ->setUser($user)
-            ->setNumber(5);
+            ->setNumber(10);
 
         $manager->persist($skill4);
 
@@ -58,7 +98,7 @@ class SkillFixtures extends Fixture implements DependentFixtureInterface
         $skill5->setTitle("API REST")
             ->setPercentage(60)
             ->setUser($user)
-            ->setNumber(6);
+            ->setNumber(11);
 
         $manager->persist($skill5);
 
@@ -66,7 +106,7 @@ class SkillFixtures extends Fixture implements DependentFixtureInterface
         $skill6->setTitle("RabbitMQ")
             ->setPercentage(30)
             ->setUser($user)
-            ->setNumber(7);
+            ->setNumber(12);
 
         $manager->persist($skill6);
 
@@ -74,7 +114,7 @@ class SkillFixtures extends Fixture implements DependentFixtureInterface
         $skill7->setTitle("Solr")
             ->setPercentage(20)
             ->setUser($user)
-            ->setNumber(8);
+            ->setNumber(13);
 
         $manager->persist($skill7);
 
@@ -82,9 +122,41 @@ class SkillFixtures extends Fixture implements DependentFixtureInterface
         $skill8->setTitle("ElasticSearch")
             ->setPercentage(20)
             ->setUser($user)
-            ->setNumber(9);
+            ->setNumber(14);
 
         $manager->persist($skill8);
+
+        $skill9 = new Skill();
+        $skill9->setTitle("SaltStack")
+            ->setPercentage(40)
+            ->setUser($user)
+            ->setNumber(15);
+
+        $manager->persist($skill9);
+
+        $skill10 = new Skill();
+        $skill10->setTitle("SQS/Q3/Cloudfront")
+            ->setPercentage(20)
+            ->setUser($user)
+            ->setNumber(16);
+
+        $manager->persist($skill10);
+
+        $skill11 = new Skill();
+        $skill11->setTitle("Docker")
+            ->setPercentage(20)
+            ->setUser($user)
+            ->setNumber(17);
+
+        $manager->persist($skill11);
+
+        $skill12 = new Skill();
+        $skill12->setTitle("Kibana")
+            ->setPercentage(20)
+            ->setUser($user)
+            ->setNumber(18);
+
+        $manager->persist($skill12);
 
         $manager->flush();
     }
